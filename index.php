a0975ef2a3e7e4a7a43372c2b733d4e45530dec0
Roosevelt <rdt-2012@hotmail.com>
2015-02-17 20:02:08 -0500

    se ha creado una tabla

--- index.php
+++ index.php
@@ -8,5 +8,23 @@
         <h2>
             Ola mundo
         </h2>
+        <table border="2px">
+            <tr>
+                <th>
+                    ola
+                </th>
+                <th>
+                    mundo
+                </th>
+            </tr>
+            <tr>
+                <th>
+                    ola
+                </th>
+                <th>
+                    mundo
+                </th>
+            </tr>
+        </table>
     </body>
 </html>
